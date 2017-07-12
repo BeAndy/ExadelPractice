f5c16b5af976bd7ace00f5e135e51f988852c8bc
Kanstantsin <dolbyone007@gmail.com>
2017-07-12 18:27:03 +0300

    meta-data interface

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const express = require('express');
-const mysql = require('mysql');
 const metaData = require('./server/routes/meta-data.js');
 
 const app = express();
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123789',
-  database: 'pick_brains_db',
-});
-connection.connect((error) => {
-  if (error) {
-    console.log('error');
-    throw error;
-  }
-  console.log('Successfull connection to the server');
-});
 
 app.use('/api/meta-data/', metaData);
 
--- server/models/meta-data-queries.js
+++ server/models/meta-data-queries.js
@@ -1,4 +1,6 @@
 exports.englishLevelsQuery = 'SELECT english_lvl.lvl FROM english_lvl';
 exports.locationQuery = 'SELECT location.city FROM location';
 exports.skillsQuery = 'SELECT skills.skill_name FROM skills';
-exports.candidateStatusQuery = 'SELECT candidate_status.status FROM candidate_status';
+exports.candidateStatusesQuery = 'SELECT * FROM candidate_status';
+exports.otherSkillsQuery = 'SELECT * FROM other_skills';
+exports.vacancyStatusesQuery = 'SELECT * FROM vacancy_status';
